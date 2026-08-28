d2e2c6412df6882086264e5ea20149e43b3171e0
Shubham <golubhandari405@gmail.com>
2026-08-28 16:31:43 +0000

    feat: add validation on the data submitted by the user using the express-validator

---
+++ middleware/movieValidator.js
@@ -0,0 +1,31 @@
+const { body } = require("express-validator");
+const validateRequest = require("./validateRequest");
+
+
+const movieRules = [
+    body('title')
+    .trim()
+    .notEmpty().withMessage('Title is required')
+    .isLength({max: 100}).withMessage('Title cannot exceed 100 characters'),
+
+    body('releaseDate')
+    .isInt({min: 1888, max: new Date().getFullYear() + 5})
+    .withMessage('Please enter a valid release year'),
+
+    body('director')
+    .isInt().withMessage('Director ID must be an integer'),
+
+    body('imageUrl')
+    .optional({checkFalsy: true })
+    .isURL().withMessage('Image url must be a valid web link'),
+
+    body('genres')
+    .isArray({min: 1}).withMessage('Genres must be a valid array with atleast one value'),
+
+    body('genres.*')
+    .isInt().withMessage('Each genre ID must be an integer'),
+
+    validateRequest
+]
+
+module.exports = { movieRules };
--- routes/movieRouter.js
+++ routes/movieRouter.js
@@ -1,11 +1,12 @@
 const Router = require("express");
 const validateUser = require("express-validator");
 const movieController = require("../controllers/movieController");
+const { movieRules } = require("../middleware/movieValidator");
 
 const movieRouter = Router();
 
 movieRouter.get("/new", movieController.getCreateForm);
-movieRouter.post("/new", movieController.postCreateForm);
+movieRouter.post("/new", movieRules, movieController.postCreateForm);
 movieRouter.get("/:id", movieController.getMovieDetails);
 
 
